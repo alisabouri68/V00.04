5bba5c8e396c1efb3c5defefcaa434007a31ddd0
aliSabouri68 <alisabouri6857@gmail.com>
2025-07-13 09:29:21 +0330

    c

--- V00.04/src/BOX/BOX_nav.tsx
+++ V00.04/src/BOX/BOX_nav.tsx
@@ -1,5 +1,5 @@
-import NavigatorVar1 from "../COMP/RCOM_navigator_VAR.01_V00.04";
-import NavigatorVar2 from "../COMP/RCOM_navigator_VAR.02_V00.04";
+import NavigatorVar1 from "../COMP/RCMP_navigator_VAR.01_V00.04";
+import NavigatorVar2 from "../COMP/RCMP_navigator_VAR.02_V00.04";
 const Sidebar = () => {
   return (
     <>
