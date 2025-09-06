6f815caeffba156f8e9c8893fb3b4860c6391c91
alisabouri68 <alisabouri6857@gmail.com>
2025-09-06 14:39:18 +0330

    test

--- src/LAYOUT/index.tsx
+++ src/LAYOUT/index.tsx
@@ -12,7 +12,7 @@ export default function index({ children }: { children: ReactNode }) {
           <nav className="min-w-20 max-w-20 hidden lg:flex h-full px-0.5 py-1 ">
             <NavigatorVar1 />
           </nav>
-          <div className="w-full h-full flex overflow-hidden">{children}</div>
+          <div className="w-full h-full flex overflow-hidden p-1">{children}</div>
         </div>
         <nav className="w-full lg:hidden flex min-h-20 max-h-20">
           <NavigatorVar2 />
