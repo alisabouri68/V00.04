a7b18e8d1fa15f3d645d2ac3273b7d10c47c02e2
alisabouri68 <alisabouri6857@gmail.com>
2025-09-08 15:40:48 +0330

    test

--- src/COMP/RCMP_component_1/index.tsx
+++ src/COMP/RCMP_component_1/index.tsx
@@ -1,10 +1,10 @@
 import Button from "COMP/RCMP_button_V00.04";
-import jsonData from "./.schm.json?raw";
-import { useGlobalState } from "RDUX/dynamanContext";
+// import jsonData from "./.schm.json?raw";
+// import { useGlobalState } from "RDUX/dynamanContext";
 function Index() {
-  const { globalState } = useGlobalState();
-  const theme = globalState?.ENVI_glob?.glob_Packet_1?.filed_1?.value;
-  const parsedData = JSON.parse(jsonData);
+  // const { globalState } = useGlobalState();
+  // const theme = globalState?.ENVI_glob?.glob_Packet_1?.filed_1?.value;
+  // const parsedData = JSON.parse(jsonData);
 
   return (
     <div className="w-full flex">
