e7c3ea8765f46acaea02c972e0e7a9ea0e52ed8d
alisabouri68 <alisabouri6857@gmail.com>
2025-09-08 15:42:04 +0330

    demo

--- src/CONS/CONS_gasma/index.tsx
+++ src/CONS/CONS_gasma/index.tsx
@@ -1,12 +1,12 @@
 import Auxilary from "../../BOX/BOX_auxiliary"
-import Action from "../../BOX/BOX_action"
-import Sheet1 from "../../BNDL/WRAP_gasma/sheet1"
-import Sheet2 from "../../BNDL/WRAP_gasma/sheet2"
+// import Action from "../../BOX/BOX_action"
+// import Sheet1 from "../../BNDL/WRAP_gasma/sheet1"
+// import Sheet2 from "../../BNDL/WRAP_gasma/sheet2"
 const Index = () => {
   return (
     <>
       <main className="flex w-full lg:w-9/12 h-full py-0 px-0.5 lg:py-1">
-        <Action ActionMenu={<Sheet1/>} ActionContent={<Sheet2/>} />
+        {/* <Action ActionMenu={<Sheet1/>} ActionContent={<Sheet2/>} /> */}
       </main>
       <div className="w-full hidden lg:flex lg:w-3/12 px-0.5 py-1"><Auxilary /></div>
     </>
