7b513d1ba75c1cd02a6bcb6e0f335d948f251b1e
ali emami <alisabouri6857@gmail.com>
2025-09-17 14:43:12 +0330

    Update index_action_menu.tsx

--- src/BNDL/WRAP_home/index_action_menu.tsx
+++ src/BNDL/WRAP_home/index_action_menu.tsx
@@ -1,9 +1,6 @@
 
 import { SiHomeassistant } from "react-icons/si";
-import Image from "WIDG/RWID_image_V00.04";
-import Icon from "WIDG/RWID_icon_V0004";
-import Logo from "../../ASST/images/logo-dash.svg"
-import Pic from "../../ASST/images/email-logo.png"
+import Icon from "COMP/RCMP_bioicon_V0004";
 import { CgAnchor } from "react-icons/cg";
 import { CgEyeAlt } from "react-icons/cg";
 import { CgWindows } from "react-icons/cg";
@@ -14,53 +11,51 @@ function index_action_menu() {
         <>
 
             <div className="flex items-end justify-around gap-3 px-3 w-full">
-                <div className="flex flex-col gap-5 cursor-pointer">  <Image styles={{ borderRadius: "10%", boxShadow: "", objectFit: "cover" }} logic={{ src: Pic, alt: "Pic", lazy: true }} geo={{ width: "50px", height: "50px" }} />Avatar</div>
-                <div className="flex flex-col gap-5 cursor-pointer"> <Image styles={{ borderRadius: "10%", boxShadow: "", objectFit: "cover" }} logic={{ src: Logo, alt: "logo", lazy: true }} geo={{ width: "50px", height: "50px" }} />Image </div>
                 <div className="flex flex-col gap-5 cursor-pointer">
                     <Icon
                         geo={{ width: "50px", height: "50px" }}
-                        logic={{ id: "WIDG_icon_V00.04_101010", isAssistant: false, addToLocall: false }}
-                        style={{ cursor: "pointer", color: "blue", fontSize: "50px", margin: "0px" }} children={<SiHomeassistant />} /> Icon </div>
+                        logic={{ id: "icon 1", isAssistant: false, addToLocall: false }}
+                        style={{ cursor: "pointer", color: "blue", fontSize: "50px", margin: "0px" }} children={<SiHomeassistant />} /> Icon1 </div>
                 <div className="flex flex-col gap-5 cursor-pointer">
                     <Icon
                         geo={{ width: "50px", height: "50px" }}
-                        logic={{ id: "WIDG_icon_V00.04_202020", isAssistant: false, addToLocall: false }}
+                        logic={{ id: "icon 2", isAssistant: false, addToLocall: false }}
                         style={{ cursor: "pointer", color: "lightblue", fontSize: "50px", margin: "0px" }}
                     >
                         <CgAnchor />
                     </Icon>
-                    Icon
+                    Icon2
                 </div>
 
                 <div className="flex flex-col gap-5 cursor-pointer">
                     <Icon
                         geo={{ width: "50px", height: "50px" }}
-                        logic={{ id: "WIDG_icon_V00.04_303030", isAssistant: false, addToLocall: false }}
+                        logic={{ id: "icon 3", isAssistant: false, addToLocall: false }}
                         style={{ cursor: "pointer", color: "green", fontSize: "50px", margin: "0px" }}
                     >
                         <CgEyeAlt />
                     </Icon>
-                    Icon
+                    Icon3
                 </div>
                 <div className="flex flex-col gap-5 cursor-pointer">
                     <Icon
                         geo={{ width: "50px", height: "50px" }}
-                        logic={{ id: "WIDG_icon_V00.04_404040", isAssistant: false, addToLocall: false }}
+                        logic={{ id: "icon 4", isAssistant: false, addToLocall: false }}
                         style={{ cursor: "pointer", color: "purple", fontSize: "50px", margin: "0px" }}
                     >
                         <CgWindows />
                     </Icon>
-                    Icon
+                    Icon4
                 </div>
-                <div className="flex flex-col gap-5 cursor-pointer"  style={{color:"purple"}}>
+                <div className="flex flex-col gap-5 cursor-pointer" >
                     <Icon
                         geo={{ width: "50px", height: "50px" }}
-                        logic={{ id: "WIDG_icon_V00.04_505050", isAssistant: false, addToLocall: false }}
+                        logic={{ id: "icon 5", isAssistant: false, addToLocall: false }}
                         style={{ cursor: "pointer", color: "red", fontSize: "50px", margin: "0px" }}
                     >
                         <CgSmile />
                     </Icon>
-                    Icon
+                    Icon5
                 </div>
 
             </div>
