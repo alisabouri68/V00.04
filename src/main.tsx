bbb9e5c9c79832eeb431ef3a30e1ecb9ece9fce5
aliEmami <alisabouri6857@gmail.com>
2025-11-15 14:29:31 +0330

    ttyyuuii

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./ASST/CSS/index.css";
+import "./ASST/css/index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { dynaStore } from "./RDUX/dynamanContext/dynaStore.ts";
